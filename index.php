62bea111cd5a848bb0074154542cc0a3c567d168
Taylor Case <tcase@cloud4wi.com>
2017-04-18 16:52:37 +0200

    VOL-689 update index.php and create cp_index.php for control panel side

--- index.php
+++ index.php
@@ -10,27 +10,77 @@ $salutation = NULL;
 $firstName = NULL;
 $lastName = NULL;
 
-/* Do everything if the user has a session key */
-if (isset($_GET['sk']) && !empty($_GET['sk'])) {
+?>
 
-    /* Checking the session key and then getting the session data */
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $getSessionDataUrl
-    ));
-    $result = curl_exec($curl);
-    $session = json_decode($result, true);
+<?php
 
-    $c4w = array();
-    if (isset($session['data']) && !empty($session['data'])) {
-        $c4w = $session['data'];
+/**
+ * @description: This function will not take any parameters,
+ * it will just call the C4W API and return the data.  If the
+ * SK is not set in the URL or if the API call is not a successful one,
+ * then it will return a false boolean. Otherwise it will return the object
+ *
+ * @return object;
+ */
+
+function callApi() {
+    $sk = $_GET['sk'];
+    $cookie = $_COOKIE['c4w'];
+
+    // Check to see if any of the places where SK is set exists
+    if(isset($sk) && !empty($sk)) {
+
+        // Concatenate URL
+        $getSessionDataUrl = C4W_ENV_CONTROLPANEL_URL . C4W_ENV_MYAPPS_GET_SK_URL . $sk;
+
+        // Call C4W API
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $getSessionDataUrl
+        ));
+        $result = curl_exec($curl);
+        $session = json_decode($result, true);
+
+        // Create customer variable
+        $c4w = array();
+        if(isset($session['data']) && !empty($session['data'])) {
+            $c4w = $session['data'];
+        }
+
+        // Return false if status of API call is not success
+        if($session['status'] == 'success') {
+            return $c4w;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
     }
+}
 
-    $user = array();
-    if (isset($c4w['customer']) && !empty($c4w['customer'])) {
-        $user = $c4w['customer'];
-    }
+?>
+
+<?php
+/**
+ * This function is where the magic will happen -
+ * It will take all the data that we have an process it,
+ * and based on certain conditionals it will produce a
+ * string for the end-customer
+ *
+ * First, we will check for a cookie in the browser
+ * If it's there, we will check how many times the user has been here
+ *
+ * Then, we will provide a coupon based on whether or not we offered
+ *
+ * If there is no cookie, then we just say "Hello" with a description
+ * of the coffee shop, and with their name welcoming them for the first time
+ *
+ * @param $data
+ * @return $string
+ */
+function setSalutation($data) {
+    $user = $data['user'];
 
     if (isset($user['first_name']) && !empty($user['first_name'])) {
         $firstName = $user['first_name'];
@@ -47,9 +97,16 @@ if (isset($_GET['sk']) && !empty($_GET['sk'])) {
             $salutation = "Ms.";
         }
     }
-
 }
 
+?>
+
+<?php
+
+$data = callApi();
+if($data != false) {
+    $salutation = setSalutation($data);
+}
 $content = '<span>Hello World</span>!';
 
 $gotUserData = $salutation || $firstName || $lastName;
@@ -57,8 +114,11 @@ $gotUserData = $salutation || $firstName || $lastName;
 if ($gotUserData) {
     $content = "<span>Hello world</span>, $salutation $firstName $lastName!";
 }
-
 ?>
+
+<script>
+    console.log(<?php echo json_encode($data); ?>);
+</script>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -104,8 +164,7 @@ if ($gotUserData) {
 </footer>
 
 <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.2.1.js"
-        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.js" crossorigin="anonymous"></script>
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js" crossorigin="anonymous"></script>
@@ -123,7 +182,6 @@ if ($gotUserData) {
     MYAPPS.renderNavbar(params);
     document.body.style.marginTop = "50px";
     document.body.style.fontFamily = '"Times New Roman", "Times", "Georgia", Helvetica, Arial, sans-serif';
-
 </script>
 </body>
 </html>
