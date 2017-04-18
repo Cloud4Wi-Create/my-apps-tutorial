8df5c57bf5a0609897d0b4c96c03955dd0574baf
Taylor Case <tcase@cloud4wi.com>
2017-04-18 18:02:24 +0200

    VOL-689 create basic api for getting/settings messages into a txt file for the control panel side

---
+++ api.php
@@ -0,0 +1,36 @@
+<?php
+function set_messages($pre, $post) {
+    $file = "messages.txt";
+
+    $json = array("pre" => $pre, "post" => $post);
+
+    file_put_contents($file, json_encode($json));
+
+    return $json;
+}
+
+function get_messages() {
+    $file = "messages.txt";
+
+    $json = json_decode(file_get_contents($file), true);
+
+    return $json;
+}
+
+switch ($_GET["action"]) {
+    case "get_messages":
+        $value = get_messages();
+        break;
+    case "set_messages":
+        if(isset($_GET["post"]) && isset($_GET["pre"])) {
+            $value = set_messages($_GET["pre"], $_GET["post"]);
+        } else {
+            $value = "Missing argument";
+        }
+        break;
+
+    default:
+        $value = "Incorrect action call";
+}
+
+exit(json_encode($value));
--- cp_index.php
+++ cp_index.php
@@ -1,1 +1,94 @@
-hello world
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Coffee Works</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"  crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.css"  crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+<div class="brand">Coffee Works</div>
+<div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
+<div class="container">
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                <h2 class="intro-text text-center">Parameters</h2>
+                <hr>
+                <img class="img-responsive img-border img-left" src="img/DeathtoStock_Wired1.jpg" alt="" height="100" width="100">
+                <hr class="visible-xs">
+                <form id="app_parameters">
+                    <div class="form-group">
+                        <label for="pre_auth_message">Email address</label>
+                        <input type="text" class="form-control" id="pre_auth_message" placeholder="i.e. Welcome to the coffee shop!">
+                    </div>
+                    <div class="form-group">
+                        <label for="post_auth_message">Password</label>
+                        <input type="text" class="form-control" id="post_auth_message" placeholder="i.e. Welcome {name} to the coffee shop!">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.2.1.js"
+        crossorigin="anonymous"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $.ajax({
+            url:'/api.php',
+            data: {
+                pre:pre,
+                post:post,
+                action:'set_messages'
+            },
+            success:function(data) {
+                console.log(data);
+            },
+            error:function(data) {
+                console.log(data);
+            },
+            method:'POST'
+        })
+    });
+
+    var form = $(document);
+
+    form.on('submit', '#app_parameters', function(e) {
+        e.preventDefault();
+
+        var pre = $("#pre_auth_message").val();
+        var post = $("#post_auth_message").val();
+
+        $.ajax({
+            url:'/api.php',
+            data: {
+                pre:pre,
+                post:post,
+                action:'set_messages'
+            },
+            success:function(data) {
+                console.log(data);
+            },
+            error:function(data) {
+                console.log(data);
+            },
+            method:'POST'
+        })
+    });
+
+</script>
+</body>
+</html>
