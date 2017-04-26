23e3ba2c713f7715cb1c76485e65d118d3bdf923
Taylor Case <tcase@cloud4wi.com>
2017-04-26 17:40:18 +0200

    VOL-689 never show variable key in view

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ $data = callApi();
         var arr = string.split(/{|}/);
 
         var processedArr = arr.map(function(element) {
-            element = !!object[element] ? object[element] : element;
+            element = !!object[element] ? object[element] : "";
             return element;
         });
 
